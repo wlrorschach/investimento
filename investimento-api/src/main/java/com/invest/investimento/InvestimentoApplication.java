af1441d3a64d3e2c43b763d47a4f1881d7748e6e
Wiliam <wlrorschach@gmail.com>
2020-01-28 13:02:18 -0300

    Criacao do projeto

---
+++ investimento-api/src/main/java/com/invest/investimento/InvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.invest.investimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentoApplication.class, args);
+	}
+
+}
---
+++ investimento-api/src/test/java/com/invest/investimento/InvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invest.investimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
